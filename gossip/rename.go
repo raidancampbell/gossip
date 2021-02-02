6845bae282103a8011b7305a11f4691d075681ef
R. Aidan Campbell <aidan.campbell@gmail.com>
2021-02-01 20:47:58 -0700

    fix bug in renaming

--- gossip/rename.go
+++ gossip/rename.go
@@ -10,7 +10,7 @@ import (
 // rename on !rename
 var rename = &SyncTrigger{
 	Cond: func(g *Bot, msg *irc.Message) bool {
-		return msg.Command == irc.PRIVMSG && len(msg.Params) == 2 && msg.Params[1] == "!rename" && len(strings.Split(msg.Params[1], " ")) == 2
+		return msg.Command == irc.PRIVMSG && len(msg.Params) == 2 && strings.HasPrefix(msg.Params[1], "!rename") && len(strings.Split(msg.Params[1], " ")) == 2
 	},
 	Act: func(g *Bot, msg *irc.Message) bool {
 		logrus.Infof("renaming to %s", strings.Split(msg.Params[1], " ")[1])
